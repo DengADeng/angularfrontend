0d69ed3a627d341535cfd61b937e3a3e7090191b
JiajunLin <ljj@gmail.com>
2020-10-02 16:38:22 -0500

    fix prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   API_URL: 'http://18.223.108.196:3000/',
 };
 
